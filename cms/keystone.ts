d863e8f2a5f23efc6b99f9bb92db03646e258795
Hyun Kim <justinkimkorea@gmail.com>
2022-10-15 23:37:43 +1200

    chore: enable cors from all sources

--- cms/keystone.ts
+++ cms/keystone.ts
@@ -17,7 +17,11 @@ import { withAuth, session } from "./auth";
 export default withAuth(
   // Using the config function helps typescript guide you to the available options.
   config({
-    // the db sets the database provider - we're using sqlite for the fastest startup experience
+    server: {
+      cors: {
+        origin: ["*"],
+      },
+    },
     db: {
       // provider: 'sqlite',
       // url: 'file:./keystone.db',
